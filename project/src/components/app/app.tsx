3fe322fa6d49933d42eeb7983c6d4ff5dbf0465d
Alexander <alex.kadoh@mail.ru>
2021-10-04 20:30:14 +1000

    create main component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from "../main/main";
+import {CSSProperties} from "react";
+
+type AppProps = {
+  displayStyle: CSSProperties;
+  tabIndex: number;
+  spanWidth80: CSSProperties;
+  spanWidth100: CSSProperties;
+}
+
+function App ({displayStyle, tabIndex, spanWidth80, spanWidth100}: AppProps): JSX.Element {
+  return  (<Main displayStyle={displayStyle}
+                 tabIndex={tabIndex}
+                 spanWidth80={spanWidth80}
+                 spanWidth100={spanWidth100}
+  />);
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const MainSetting = {
+  DISPLAY_STYLE: {
+    display: 'none'
+  },
+  TAB_INDEX: 0,
+  SPAN_WIDTH_80: {
+    width: '80%'
+  },
+  SPAN_WIDTH_100: {
+    width: '100%'
+  },
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App displayStyle={MainSetting.DISPLAY_STYLE}
+         tabIndex={MainSetting.TAB_INDEX}
+         spanWidth80={MainSetting.SPAN_WIDTH_80}
+         spanWidth100={MainSetting.SPAN_WIDTH_100}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
