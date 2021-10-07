fb80e46d598ca46487e209cf9031163eb6ec3e2c
Alexander <alex.kadoh@mail.ru>
2021-10-07 20:35:43 +1000

    change Route to PrivateRoute for Favorites component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,11 @@
-import {Switch, Route, BrowserRouter} from 'react-router-dom';
-import AppRoute from '../../const';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import Main from '../main/main';
 import Favorites from '../favorites/favorites';
 import Login from '../login/login';
 import Property from '../property/property';
 import NotFound from '../404-not-found/404';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   countStay: number;
@@ -17,18 +18,27 @@ function App ({countStay}: AppProps): JSX.Element {
         <Route exact path={AppRoute.Main}>
           <Main countStay={countStay} />
         </Route>
+
         <Route exact path={AppRoute.SignIn}>
           <Login />
         </Route>
-        <Route exact path={AppRoute.Favorites}>
-          <Favorites />
-        </Route>
+
+        <PrivateRoute
+          exact
+          path={AppRoute.Favorites}
+          render={() => <Favorites />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
+
         <Route exact path={AppRoute.Room}>
           <Property />
         </Route>
+
         <Route>
           <NotFound />
         </Route>
+
       </Switch>
     </BrowserRouter>
   );
