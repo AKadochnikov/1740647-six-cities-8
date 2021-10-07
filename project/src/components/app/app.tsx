df0c972924723350e352b265b138b9f809b2ac3a
Alexander <alex.kadoh@mail.ru>
2021-10-07 20:09:19 +1000

    create 404 component, route and logo component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,37 @@
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import AppRoute from '../../const';
 import Main from '../main/main';
+import Favorites from '../favorites/favorites';
+import Login from '../login/login';
+import Property from '../property/property';
+import NotFound from '../404-not-found/404';
 
 type AppProps = {
   countStay: number;
 }
 
 function App ({countStay}: AppProps): JSX.Element {
-  return  (<Main countStay={countStay}/>);
+  return  (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <Main countStay={countStay} />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <Login />
+        </Route>
+        <Route exact path={AppRoute.Favorites}>
+          <Favorites />
+        </Route>
+        <Route exact path={AppRoute.Room}>
+          <Property />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/const.ts
@@ -0,0 +1,8 @@
+enum AppRoute {
+  Main = '/',
+  SignIn = '/login',
+  Favorites = '/favorites',
+  Room = '/offer/:id'
+}
+
+export default AppRoute;
