6a82ab264a7c7b049b5c3c962dd2a1034af49bc0
Alexander <alex.kadoh@mail.ru>
2021-10-21 19:35:29 +1000

    change latitude and longitude in mock location

--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -23,8 +23,8 @@ const offers: offersMocks[]  = [{
   isFavorite: false,
   isPremium: true,
   location: {
-    latitude: 52.35514938496378,
-    longitude: 4.673877537499948,
+    latitude: 52.3909553943508,
+    longitude: 4.85309666406198,
     zoom: 8,
   },
   maxAdults: 4,
@@ -57,8 +57,8 @@ const offers: offersMocks[]  = [{
   isFavorite: true,
   isPremium: false,
   location: {
-    latitude: 52.35514938496378,
-    longitude: 4.673877537499948,
+    latitude: 52.369553943508,
+    longitude: 4.85309666406198,
     zoom: 8,
   },
   maxAdults: 4,
@@ -91,8 +91,8 @@ const offers: offersMocks[]  = [{
   isFavorite: true,
   isPremium: false,
   location: {
-    latitude: 52.35514938496378,
-    longitude: 4.673877537499948,
+    latitude: 52.3909553943508,
+    longitude: 4.929309666406198,
     zoom: 8,
   },
   maxAdults: 4,
@@ -125,8 +125,8 @@ const offers: offersMocks[]  = [{
   isFavorite: true,
   isPremium: false,
   location: {
-    latitude: 52.35514938496378,
-    longitude: 4.673877537499948,
+    latitude: 52.3809553943508,
+    longitude: 4.939309666406198,
     zoom: 8,
   },
   maxAdults: 4,
