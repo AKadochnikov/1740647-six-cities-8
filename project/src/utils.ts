baa94489f1d64680cd8a28cc6a4d545419598d77
Alexander <alex.kadoh@mail.ru>
2021-10-29 19:38:24 +1000

    take adapter from last project

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -16,5 +16,47 @@ const getLocation = (currentCity: string): city => {
   return newLocation[0].city;
 };
 
+//TODO набросок адаптера
+/*static adaptToClient(event) {
+  const adaptedEvent = Object.assign(
+    {},
+    event,
+    {
+      basePrice: event['base_price'],
+      dateFrom: dayjs(event['date_from'], 'YYYY-MM-DDTHH:mm:ssZ[Z]'),
+      dateTo: dayjs(event['date_to'], 'YYYY-MM-DDTHH:mm:ssZ[Z]'),
+      isFavorite: event['is_favorite'],
+    },
+  );
+
+  delete adaptedEvent['base_price'];
+  delete adaptedEvent['date_from'];
+  delete adaptedEvent['date_to'];
+  delete adaptedEvent['is_favorite'];
+
+  return adaptedEvent;
+}
+
+static adaptToServer(event) {
+  const adaptedEvent = Object.assign(
+    {},
+    event,
+    {
+      'base_price': event.basePrice,
+      'date_from': event.dateFrom.toISOString(),
+      'date_to': event.dateTo.toISOString(),
+      'is_favorite': event.isFavorite,
+    },
+  );
+
+  delete adaptedEvent.basePrice;
+  delete adaptedEvent.dateFrom;
+  delete adaptedEvent.dateTo;
+  delete adaptedEvent.isFavorite;
+
+  return adaptedEvent;
+}
+}*/
+
 export {getRating, ucFirst, getOffers, getLocation};
 
